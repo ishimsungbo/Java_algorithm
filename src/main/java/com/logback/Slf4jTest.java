ba0373145f2e31490c5d266e2afaffc8b7e0a743
sungbo <ishimsungbo@gmail.com>
2022-06-21 11:00:06 +0900

    slf4j 설정. debug일때 파일 로그 남기기 간단히 해봄.

---
+++ src/main/java/com/logback/Slf4jTest.java
@@ -0,0 +1,17 @@
+package com.logback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Slf4jTest {
+
+    private static Logger logger = LoggerFactory.getLogger(Slf4jTest.class);
+
+    public static void main(String[] args) {
+        for(int i =0 ; i < 10000 ;i++){
+            logger.debug("로그 ======================" + i);
+        }
+
+
+    }
+}
